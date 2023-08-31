581de7f9bb940bad70c9780c412f64d457a95e90
mridul sutradhar <pust.mridul@gmail.com>
2023-08-31 17:59:23 +0600

    no message

--- src/app/prd/item/item.component.ts
+++ src/app/prd/item/item.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent implements OnInit {
-
+ public contentHeader: object;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+   this.contentHeader = {
+      headerTitle: 'Bootstrap Tables',
+      actionButton: true,
+      breadcrumb: {
+        type: '',
+        links: [
+          {
+            name: 'Home',
+            isLink: true,
+            link: '/'
+          },
+          {
+            name: 'Table',
+            isLink: true,
+            link: '/'
+          },
+          {
+            name: 'Bootstrap Tables',
+            isLink: false
+          }
+        ]
+      }
+    };
   }
-
 }
--- src/app/prd/item/item.module.ts
+++ src/app/prd/item/item.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ItemRoutingModule } from './item-routing.module';
 import { ItemComponent } from './item.component';
+import { RouterModule } from '@angular/router';
+import { CoreCommonModule } from '@core/common.module';
+import { CardSnippetModule } from '@core/components/card-snippet/card-snippet.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContentHeaderModule } from 'app/layout/components/content-header/content-header.module';
 
 
 @NgModule({
   declarations: [
     ItemComponent
   ],
-  imports: [
-    CommonModule,
-    ItemRoutingModule
-  ]
+  imports: [ItemRoutingModule, NgbModule, ContentHeaderModule, CardSnippetModule, CoreCommonModule]
 })
 export class ItemModule { }
